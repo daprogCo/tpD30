487322fb4bb4df1aceb9040af2787ccf3d337333
root <root@srv484762.hstgr.cloud>
2025-02-02 01:13:32 +0000

    Small change

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from bizou nginx!
+          Hello from nginx!
         </p>
         <a
           className="App-link"
