b6ae39217bde2598c0c5e4eb26a1f57a02427ff9
David S <145632653+daprogCo@users.noreply.github.com>
2025-02-01 21:36:23 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from nginx!
+          Hello from AWS!
         </p>
         <a
           className="App-link"
